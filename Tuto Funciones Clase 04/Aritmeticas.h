3856d880ee7469e5d8851b8c1aabc3b4f2a06604
jadesawada <carla.alopez@hotmail.com>
2019-08-30 15:13:55 -0300

    Clase 04 Funciones y Bibliotecas

--- Tuto Funciones Clase 04/Aritmeticas.h
+++ Tuto Funciones Clase 04/Aritmeticas.h
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
+
+
+/** \brief Pide un numero y lo valida en un rango especifico
+ *
+ * \param char[] Texto que se va a mostrar
+ * \param int El minimo valor dentro del rango
+ * \param int El maximo valor dentro del rango
+ * \return int El numero ingresado
+ *
+ */
 int pedirEntero(char[], int, int);
